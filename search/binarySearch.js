cfea78447710d66bc236cf944e8e81861aeae2e8
pnowak <piotr.nowak.25.02.1978@gmail.com>
2018-02-01 22:54:09 +0100

    binary search recursive

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -21,3 +21,27 @@ export default function binarySearch(array, item) {
 
 	return null;
 }
+
+export default function binarySearchRec(array, item) {
+	let low = 0;
+	let high = array.length - 1;
+	let a;
+
+	let mid = Math.floor((low + high) / 2);
+	let guess = array[mid];
+
+	if (guess === item) {
+		return mid;
+	} else if (guess > item) {
+		high = mid - 1;
+		a = array.splice(low, high);
+		binarySearchRec(a, item);
+	}  else if (guess < item) {
+		low = mid + 1;
+		a = array.splice(low, high);
+		binarySearchRec(a, item);
+	} else {
+		return null;
+	}
+}
+
